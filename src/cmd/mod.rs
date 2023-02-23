1c7ab6195bfe79a3ffeebb60102d5dd514fab29d
jiashiwen <jiashiwen@ZBMac-C02F34Q9MD6M.local>
2023-02-23 18:50:08 +0800

    abstract ossclient and oss descreption

--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -1,13 +1,13 @@
 mod configcmd;
+mod osscfgcmd;
 mod rootcmd;
-mod stop;
 mod start;
-
+mod stop;
 
 pub use configcmd::new_config_cmd;
+pub use osscfgcmd::new_osscfg_cmd;
 pub use rootcmd::get_command_completer;
 pub use rootcmd::run_app;
 pub use rootcmd::run_from;
 pub use start::new_start_cmd;
 pub use stop::new_stop_cmd;
-
--- src/commons/mod.rs
+++ src/commons/mod.rs
@@ -1,7 +1,9 @@
+mod fileutiles;
+mod json_operator;
 mod subcmdcompleter;
 mod sysutiles;
-mod json_operator;
-mod fileutiles;
+pub mod yamlutile;
 
 pub use subcmdcompleter::CommandCompleter;
 pub use subcmdcompleter::SubCmd;
+pub use yamlutile::*;
