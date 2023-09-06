0e26ad085aeaee1da250ac571c851b114a9d59d8
jiashiwen <jiashiwen126@126.com>
2023-09-06 12:00:53 +0800

    add inotify function

--- src/commons/inotify_utile.rs
+++ src/commons/inotify_utile.rs
@@ -159,21 +159,24 @@ impl InotifyWatcher {
                         println!("name {:?}", event.name);
                         let wd = self.map_dir_wd.get(&path).unwrap();
                         let v = wd.value();
+                        // let _ = self.inotify.watches().remove(v.clone());
+                        // self.map_wdid_dir
+                        //     .remove(&v.clone().get_watch_descriptor_id());
+                        // self.map_dir_wd.remove(&path);
 
-                        self.map_wdid_dir
-                            .remove(&v.clone().get_watch_descriptor_id());
-                        self.map_dir_wd.remove(&path);
-                        let _ = self.inotify.watches().remove(v.clone());
-                        // match self.inotify.watches().remove(v.clone()) {
-                        //     Ok(()) => {
-                        //         // self.map_wdid_dir
-                        //         //     .remove(&v.clone().get_watch_descriptor_id());
-                        //         // self.map_dir_wd.remove(&path);
-                        //     }
-                        //     Err(e) => {
-                        //         println!("{}", e)
-                        //     }
-                        // };
+                        match self.inotify.watches().remove(v.clone()) {
+                            Ok(()) => {
+                                self.map_wdid_dir
+                                    .remove(&v.clone().get_watch_descriptor_id());
+                                self.map_dir_wd.remove(&path);
+                            }
+                            Err(e) => {
+                                self.map_wdid_dir
+                                    .remove(&v.clone().get_watch_descriptor_id());
+                                self.map_dir_wd.remove(&path);
+                                println!("{}", e)
+                            }
+                        };
                     } else {
                         modify.path_type = PathType::File;
                     }
