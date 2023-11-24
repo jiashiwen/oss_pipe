c91ccd84884c10dd9a7b8290df6a0829cd7f77b9
jiashiwen <jiashiwen@ZBMac-C02F34Q9MD6M.local>
2023-11-24 15:38:53 +0800

    Yaml field serialization and deserialization parsing

--- src/commons/convert.rs
+++ src/commons/convert.rs
@@ -1,8 +1,7 @@
 use anyhow::anyhow;
-use anyhow::Ok;
 use anyhow::Result;
 
-pub fn byte_size_to_usize(byte_size: &str) -> Result<usize> {
+pub fn byte_size_str_to_usize(byte_size: &str) -> Result<usize> {
     let mut byte_str = byte_size.to_string();
     let k: usize = 1024;
     let m = k * 1024;
@@ -36,14 +35,46 @@ pub fn byte_size_to_usize(byte_size: &str) -> Result<usize> {
     };
 }
 
+pub fn byte_size_usize_to_str(byte_size: usize) -> String {
+    return match byte_size {
+        b if b < 1024 => b.to_string(),
+        b if b >= 1024 && b < 1024 * 1024 => {
+            let size = b / 1024;
+            let mut size_str = size.to_string();
+            size_str.push('k');
+            size_str
+        }
+
+        b if b >= 1024 * 1024 && b < 1024 * 1024 * 1024 => {
+            let size = b / 1024 / 1024;
+            let mut size_str = size.to_string();
+            size_str.push('m');
+            size_str
+        }
+
+        _ => {
+            let size = byte_size / 1024 / 1024 / 1024;
+            let mut size_str = size.to_string();
+            size_str.push('g');
+            size_str
+        }
+    };
+}
 #[cfg(test)]
 mod test {
-    use crate::commons::byte_size_to_usize;
+    use crate::commons::{byte_size_str_to_usize, byte_size_usize_to_str};
 
     //cargo test commons::convert::test::test_byte_size_to_usize -- --nocapture
     #[test]
     fn test_byte_size_to_usize() {
-        let r = byte_size_to_usize("12g");
+        let r = byte_size_str_to_usize("12g");
+        println!("{:?}", r);
+    }
+
+    //cargo test commons::convert::test::test_byte_size_usize_to_str -- --nocapture
+    #[test]
+    fn test_byte_size_usize_to_str() {
+        let r = byte_size_usize_to_str(1073741823);
         println!("{:?}", r);
     }
 }
